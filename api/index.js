703357234cf31f893df55ea0314a67d0c88cf921
IbnuShemsudin <musteshemsu@gmail.com>
2026-03-04 18:42:14 -0800

    fix cors preflight

--- api/index.js
+++ api/index.js
@@ -22,6 +22,9 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+
+app.options("*", cors());
+
 app.use(express.json()); 
 
 // 3. DATABASE CONNECTION (Optimized for Serverless)
